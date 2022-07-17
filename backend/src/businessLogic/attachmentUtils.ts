4a08dfa0ccf86e8d6d88a272ae5bdb87cb6a4830
boyzipvn <manhvv1993@gmail.com>
2022-07-18 00:34:11 +0900

    refector code

--- backend/src/businessLogic/attachmentUtils.ts
+++ backend/src/businessLogic/attachmentUtils.ts
@@ -1,5 +1,6 @@
 import * as AWS from 'aws-sdk'
 import * as AWSXRay from 'aws-xray-sdk'
+import { createLogger } from '../utils/logger'
 
 const XAWS = AWSXRay.captureAWS(AWS)
 const logger = createLogger("attachmentUtils")
